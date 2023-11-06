351ac21541c5e63f0b0b90194f3a2bbf5cd1da1c
Danilo Augusto <dandevweb@gmail.com>
2023-11-06 08:30:52 -0300

    CRM-1: should be able to register a new user in the system

---
+++ tests/Feature/Livewire/Auth/RegisterTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Livewire\Auth\Register;
+use Livewire\Livewire;
+
+use function Pest\Laravel\{assertDatabaseCount, assertDatabaseHas};
+
+it('should render the component', function () {
+    Livewire::test(Register::class)
+        ->assertOk();
+});
+
+it('should be able to register a new user in the system', function () {
+    Livewire::test(Register::class)
+        ->set('name', 'John Doe')
+        ->set('email', 'joe@joe.com')
+        ->set('email_confirmation', 'joe@joe.com')
+        ->set('password', 'password')
+        ->call('submit')
+        ->assertHasNoErrors();
+
+    assertDatabaseHas('users', [
+        'name'  => 'John Doe',
+        'email' => 'joe@joe.com'
+    ]);
+
+    assertDatabaseCount('users', 1);
+});
