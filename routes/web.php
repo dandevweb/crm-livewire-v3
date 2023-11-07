7305375be68b1e96177cdb463760b0fe65e7ed51
Danilo Augusto <dandevweb@gmail.com>
2023-11-07 05:31:53 -0300

    CRM-1: register form front-end

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Livewire\Welcome;
+use App\Livewire\Auth\Register;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Welcome::class);
+Route::get('register', Register::class)->name('auth.register');
+Route::get('logout', fn () => auth()->logout())->name('auth.logout');
