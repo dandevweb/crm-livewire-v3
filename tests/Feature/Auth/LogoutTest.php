a30d8a30875f11b46069c258753d849928068e1d
Danilo Augusto <dandevweb@gmail.com>
2023-11-10 09:10:15 -0300

    CRM-3: be able to logout of the application

---
+++ tests/Feature/Auth/LogoutTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\User;
+use Livewire\Livewire;
+
+use App\Livewire\Auth\Logout;
+
+use function Pest\Laravel\actingAs;
+
+it('should be able to logout of the application', function () {
+    $user = User::factory()->create();
+
+    actingAs($user);
+
+    Livewire::test(Logout::class)
+        ->call('logout')
+        ->assertRedirect(route('login'));
+
+
+    expect(auth())
+        ->guest()
+        ->toBeTrue();
+});
