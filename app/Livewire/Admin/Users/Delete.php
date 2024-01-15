225fb1eeac267a3e1794a7f1fff9de57df886050
Danilo Augusto <dandevweb@gmail.com>
2024-01-15 20:32:26 -0300

    CRM-17: backend: confirming before deletion

--- app/Livewire/Admin/Users/Delete.php
+++ app/Livewire/Admin/Users/Delete.php
@@ -3,12 +3,18 @@
 namespace App\Livewire\Admin\Users;
 
 use App\Models\User;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 use Illuminate\Contracts\View\View;
 
 class Delete extends Component
 {
     public User $user;
+
+    #[Validate(['required', 'confirmed'])]
+    public string $confirmation = 'DART VADER';
+
+    public ?string $confirmation_confirmation = null;
 
     public function render(): View
     {
@@ -17,7 +23,9 @@ class Delete extends Component
 
     public function destroy(): void
     {
+        $this->validate();
         $this->user->delete();
+
         $this->dispatch('user::deleted');
     }
 }
--- tests/Feature/Admin/UserManagement/DeleteUserTest.php
+++ tests/Feature/Admin/UserManagement/DeleteUserTest.php
@@ -4,7 +4,7 @@ use App\Models\User;
 use Livewire\Livewire;
 use App\Livewire\Admin;
 
-use function Pest\Laravel\{actingAs, assertSoftDeleted};
+use function Pest\Laravel\{actingAs, assertSoftDeleted, assertNotSoftDeleted};
 
 it('should be able to delete a user', function () {
     $user        = User::factory()->create();
@@ -13,9 +13,25 @@ it('should be able to delete a user', function () {
     actingAs($user);
 
     Livewire::test(Admin\Users\Delete::class, ['user' => $forDeletion])
+        ->set('confirmation_confirmation', 'DART VADER')
         ->call('destroy')
         ->assertDispatched('user::deleted');
 
 
     assertSoftDeleted('users', ['id' => $forDeletion->id]);
+});
+
+it('should have a confirmation before the deletion', function () {
+    $user        = User::factory()->create();
+    $forDeletion = User::factory()->create();
+
+    actingAs($user);
+
+    Livewire::test(Admin\Users\Delete::class, ['user' => $forDeletion])
+        ->call('destroy')
+        ->assertHasErrors(['confirmation' => 'confirmed'])
+        ->assertNotDispatched('user::deleted');
+
+    assertNotSoftDeleted('users', ['id' => $forDeletion->id]);
+
 });
