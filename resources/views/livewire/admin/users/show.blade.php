b40ec2bebdc6b305d8c8ec859963bd79fdc302d7
dandevweb <dandevweb@gmail.com>
2024-01-18 21:29:11 -0300

    CRM-16: displaying the information inside a drawer intead of a modal

--- resources/views/livewire/admin/users/show.blade.php
+++ resources/views/livewire/admin/users/show.blade.php
@@ -1,5 +1,4 @@
-<div>
-    <x-modal wire:model="modal" :title="$user?->name" separator>
+    <x-drawer wire:model="modal" :title="$user?->name" class="w-1/3 p-4" separator right>
         @if ($user)
             <div class="space-y-2">
                 <x-input readonly label="Name" :value="$user->name" />
@@ -14,6 +13,4 @@
         <x-slot:actions>
             <x-button label="Cancel" x-on:click="$wire.modal = false" />
         </x-slot:actions>
-    </x-modal>
-
-</div>
+    </x-drawer>
