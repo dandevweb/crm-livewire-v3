74bf72a468637c0f461d9f9d75dfe9ce2d4351cc
dandevweb <danilo.pacheco@soitic.com>
2024-03-06 20:32:21 -0300

    crm-21: updateing packages

--- resources/views/livewire/customers/create.blade.php
+++ resources/views/livewire/customers/create.blade.php
@@ -1,6 +1,5 @@
 <x-drawer wire:model="modal" title="Create Customer" class="w-1/3 p-4" separator right>
     <x-form wire:submit='save' id="create-customer-form">
-        <hr class="my-5">
         <div class="space-y-2">
             <x-input label="Name" wire:model="name" />
             <x-input label="Email" wire:model="email" />
